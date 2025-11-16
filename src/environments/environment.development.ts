eb737ee6989069242a9493cdba5a6410af0ca3e3
lin.pengpeng <lin.pengpeng@mayo.edu>
2025-11-15 20:28:24 -0600

    Set production flag to false in development environment configuration

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: true,
+    production: false,
     clientId: '483a2857-ef87-4f5f-b9b5-8cdf2c031586',
     tenantId: 'a25fff9c-3f63-4fb2-9a8a-d9bdd0321f9a',
     redirectUri: 'http://localhost:4200/'
