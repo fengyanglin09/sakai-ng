e65eca45c1961ac3cc86485f6d8007df39d56ce4
lin.pengpeng <lin.pengpeng@mayo.edu>
2025-11-15 21:21:54 -0600

    Update route guard activation based on production environment

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
 import { AppLayout } from './app/layout/component/app.layout';
 import { MsalGuard } from '@azure/msal-angular';
+import { environment } from './environments/environment';
 
 
 export const appRoutes: Routes = [
     {
         path: '',
         component: AppLayout,
-        canActivate: [MsalGuard],
+        canActivate: environment.production ? [] : [MsalGuard],
         children: [
             { path: '', redirectTo: 'pages/specialty-app-dashboard', pathMatch: 'full' },
             { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') }
