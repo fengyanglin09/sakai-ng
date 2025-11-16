1d4abc7deb501eaffbc76dc78781b04babca87e4
lin.pengpeng <lin.pengpeng@mayo.edu>
2025-11-15 20:30:34 -0600

    Update authority URL in MSAL configuration with specific tenant ID

--- src/app/core/auth/auth.config.ts
+++ src/app/core/auth/auth.config.ts
@@ -7,7 +7,7 @@ export function msalInstanceFactory() {
     return new PublicClientApplication({
         auth: {
             clientId: environment.clientId,
-            authority: `https://login.microsoftonline.com/${environment.tenantId}`,
+            authority: `https://login.microsoftonline.com/a25fff9c-3f63-4fb2-9a8a-d9bdd0321f9a`,
             redirectUri: environment.redirectUri
         },
         cache: {
