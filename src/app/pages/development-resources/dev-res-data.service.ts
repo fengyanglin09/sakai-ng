87eeb52e8c6d56a5581ca80f44cf61fdc73c17d6
lin.pengpeng <lin.pengpeng@mayo.edu>
2025-11-18 16:20:11 -0600

    Update labels for resource entries in dev-res-data.service.ts

--- src/app/pages/development-resources/dev-res-data.service.ts
+++ src/app/pages/development-resources/dev-res-data.service.ts
@@ -63,7 +63,7 @@ export class DevResDataService {
 
             ]
             } ,
-            { id: 2, label: 'Dev Tools', activeItemId: 'dev tools', name: 'dev tools', description: 'Description for Resource 2',
+            { id: 2, label: 'App Dev Tools', activeItemId: 'dev tools', name: 'dev tools', description: 'Description for Resource 2',
                 rowCounts: 5,
                 resources: [
 
@@ -136,7 +136,7 @@ export class DevResDataService {
                 ]
 
             } ,
-            { id: 3, label: 'app dev references', activeItemId: 'app dev resources', name: 'app dev resources', description: 'Description for Resource 3' } ,
+            { id: 3, label: 'App Dev References', activeItemId: 'app dev resources', name: 'app dev resources', description: 'Description for Resource 3' } ,
             ] as MayoDevelopResource[];
     }
 
