313670012aee8eeed729fd86592dbed3175f71d9
lin.pengpeng <lin.pengpeng@mayo.edu>
2025-11-19 16:17:56 -0600

    Update resource entries and enhance links in dev-res-data.service.ts

--- src/app/pages/development-resources/dev-res-data.service.ts
+++ src/app/pages/development-resources/dev-res-data.service.ts
@@ -10,7 +10,7 @@ export class DevResDataService {
     getData() {
         return [
             { id: 1, label: 'Cloud', activeItemId: 'cloud', name: 'cloud', description: 'cloud development resources' ,
-            rowCounts: 5,
+            rowCounts: 7,
             resources: [
                 {
                     label: 'Azure Cloud',
@@ -38,8 +38,8 @@ export class DevResDataService {
                 {
                     label: 'Apigee',
                     items: [
-                        { label: 'Nonprod Consumer Portal', action: () => window.open('https://nonprod.apiportal.mcc.apix.mayo.edu/') },
-                        { label: 'Prod Consumer Portal', action: () => window.open('https://apiportal.mcc.apix.mayo.edu/') },
+                        { label: 'Consumer Portal (Nonprod)', action: () => window.open('https://nonprod.apiportal.mcc.apix.mayo.edu/') },
+                        { label: 'Consumer Portal (Prod)', action: () => window.open('https://apiportal.mcc.apix.mayo.edu/') },
                         { label: 'Mayo ApigeeX Overview', action: () => window.open('https://docs.mcc.mayo.edu/docs/mcc/apigee-x/overviewx/') },
                         { label: 'Apigee Secret Update', action: () => window.open('https://docs.mcc.mayo.edu/docs/mcc/apigee-x/appsandteamsx/#api-keys') },
                         { label: 'CAF Producer/Consumer Pattern', action: () => window.open('https://mctools.sharepoint.com/teams/cloudappfactory/SitePages/ApigeeX-Connectivity-from-CAF-Projects.aspx') },
@@ -48,18 +48,41 @@ export class DevResDataService {
                 },
 
                 {
-                    label: 'Dev Tools',
+                    label: 'GCP Core Services',
                     items: [
-                        { label: 'GitHub', action: () => window.open('https://github.com') },
-                        { label: 'VS Code', action: () => window.open('https://code.visualstudio.com') },
-                        { label: 'Postman', action: () => window.open('https://postman.com') },
-                        { label: 'StackOverflow', action: () => window.open('https://stackoverflow.com') },
-                        { label: 'Docker', action: () => window.open('https://docker.com') },
-                        { label: 'VS Code', action: () => window.open('https://code.visualstudio.com') },
-                        { label: 'Postman', action: () => window.open('https://postman.com') },
-
+                        { label: 'GCP Console', action: () => window.open('https://console.cloud.google.com') },
+                        { label: 'Cloud Storage', action: () => window.open('https://console.cloud.google.com/storage') },
+                        { label: 'Compute Engine', action: () => window.open('https://console.cloud.google.com/compute') },
+                        { label: 'App Engine', action: () => window.open('https://console.cloud.google.com/appengine') },
+                        { label: 'Kubernetes Engine (GKE)', action: () => window.open('https://console.cloud.google.com/kubernetes') },
+                        { label: 'Cloud Functions', action: () => window.open('https://console.cloud.google.com/functions') },
+                        { label: 'Cloud Run', action: () => window.open('https://console.cloud.google.com/run') },
+                        { label: 'BigQuery', action: () => window.open('https://console.cloud.google.com/bigquery') },
+                        { label: 'Cloud SQL', action: () => window.open('https://console.cloud.google.com/sql') },
+                        { label: 'Firestore', action: () => window.open('https://console.cloud.google.com/firestore') },
+                        { label: 'Datastore', action: () => window.open('https://console.cloud.google.com/datastore') },
+                        { label: 'Spanner', action: () => window.open('https://console.cloud.google.com/spanner') },
+                        { label: 'Cloud Pub/Sub', action: () => window.open('https://console.cloud.google.com/cloudpubsub') },
+                        { label: 'Cloud Dataflow', action: () => window.open('https://console.cloud.google.com/dataflow') },
+                        { label: 'Cloud Dataproc', action: () => window.open('https://console.cloud.google.com/dataproc') },
+                        { label: 'Cloud Composer', action: () => window.open('https://console.cloud.google.com/composer') },
+                        { label: 'Cloud Logging', action: () => window.open('https://console.cloud.google.com/logs') },
+                        { label: 'Cloud Monitoring', action: () => window.open('https://console.cloud.google.com/monitoring') },
+                        { label: 'Cloud IAM & Admin', action: () => window.open('https://console.cloud.google.com/iam-admin') },
+                        { label: 'Cloud VPC', action: () => window.open('https://console.cloud.google.com/networking') },
+                        { label: 'Cloud DNS', action: () => window.open('https://console.cloud.google.com/net-services/dns') },
+                        { label: 'Cloud Load Balancing', action: () => window.open('https://console.cloud.google.com/net-services/loadbalancing') },
+                        { label: 'Cloud CDN', action: () => window.open('https://console.cloud.google.com/net-services/cdn') },
+                        { label: 'Secret Manager', action: () => window.open('https://console.cloud.google.com/security/secret-manager') },
+                        { label: 'Cloud Scheduler', action: () => window.open('https://console.cloud.google.com/cloudscheduler') },
+                        { label: 'Cloud Tasks', action: () => window.open('https://console.cloud.google.com/cloudtasks') },
+                        { label: 'Cloud Endpoints', action: () => window.open('https://console.cloud.google.com/endpoints') },
+                        { label: 'API Gateway', action: () => window.open('https://console.cloud.google.com/api-gateway') },
+                        { label: 'Artifact Registry', action: () => window.open('https://console.cloud.google.com/artifacts') }
                     ]
-                },
+                }
+
+
 
             ]
             } ,
@@ -136,7 +159,73 @@ export class DevResDataService {
                 ]
 
             } ,
-            { id: 3, label: 'App Dev References', activeItemId: 'app dev resources', name: 'app dev resources', description: 'Description for Resource 3' } ,
+            { id: 3, label: 'App Dev References', activeItemId: 'app dev resources', name: 'app dev resources', description: 'Description for Resource 3',
+                rowCounts: 9,
+                resources: [
+                    {
+                        label: 'Angular Development',
+                        items: [
+                            { label: 'Angular Official Docs', action: () => window.open('https://angular.io/docs') },
+                            { label: 'Angular CLI', action: () => window.open('https://angular.io/cli') },
+                            { label: 'Angular Material', action: () => window.open('https://material.angular.io/') },
+                            { label: 'Angular Blog', action: () => window.open('https://blog.angular.io/') },
+                            { label: 'Angular GitHub', action: () => window.open('https://github.com/angular/angular') },
+                            { label: 'RxJS Docs', action: () => window.open('https://rxjs.dev/guide/overview') },
+                            { label: 'TypeScript Docs', action: () => window.open('https://www.typescriptlang.org/docs/') },
+                            { label: 'Angular Awesome', action: () => window.open('https://github.com/gdi2290/awesome-angular') },
+                            { label: 'FontAwesome', action: () => window.open('https://fontawesome.com/') },
+                            { label: 'Tailwind CSS', action: () => window.open('https://tailwindcss.com/docs/guides/angular') },
+                            { label: 'Bootstrap', action: () => window.open('https://getbootstrap.com/docs/5.0/getting-started/introduction/') },
+                            { label: 'PrimeNG', action: () => window.open('https://www.primefaces.org/primeng/') },
+                            { label: 'NG Bootstrap', action: () => window.open('https://ng-bootstrap.github.io/#/home') },
+                            { label: 'Angular powered Bootstrap (ngx-bootstrap)', action: () => window.open('https://valor-software.com/ngx-bootstrap/#/') },
+                            { label: 'Highcharts', action: () => window.open('https://www.highcharts.com/') },
+                            { label: 'Highcharts Angular', action: () => window.open('https://github.com/highcharts/highcharts-angular') },
+                            { label: 'D3.js', action: () => window.open('https://d3js.org/') },
+                            { label: 'D3.js with Angular Guide', action: () => window.open('https://blog.angular-university.io/angular-d3/') },
+                            { label: 'TinyMCE', action: () => window.open('https://www.tiny.cloud/docs/integrations/angular/') }
+                        ]
+                    },
+
+                    {
+                        label: 'Spring Boot Development',
+                        items: [
+                            { label: 'Spring Official Docs', action: () => window.open('https://docs.spring.io/spring-framework/docs/current/reference/html/') },
+                            { label: 'Spring Boot Docs', action: () => window.open('https://docs.spring.io/spring-boot/docs/current/reference/html/') },
+                            { label: 'Spring Initializr', action: () => window.open('https://start.spring.io/') },
+                            { label: 'Spring Guides', action: () => window.open('https://spring.io/guides') },
+                            { label: 'Spring Blog', action: () => window.open('https://spring.io/blog') },
+                            { label: 'Spring Security Docs', action: () => window.open('https://docs.spring.io/spring-security/reference/') },
+                            { label: 'Spring Data Docs', action: () => window.open('https://docs.spring.io/spring-data/jpa/docs/current/reference/html/') },
+                            { label: 'Spring GitHub', action: () => window.open('https://github.com/spring-projects/spring-framework') },
+                            { label: 'Spring JPA', action: () => window.open('https://spring.io/projects/spring-data-jpa') },
+                            { label: 'Hibernate', action: () => window.open('https://hibernate.org/') },
+                            { label: 'Hibernate Docs', action: () => window.open('https://docs.jboss.org/hibernate/orm/current/userguide/html_single/Hibernate_User_Guide.html') },
+                            { label: 'JPA', action: () => window.open('https://jakarta.ee/specifications/persistence/') },
+                            { label: 'JPA Docs', action: () => window.open('https://jakarta.ee/specifications/persistence/3.1/jakarta-persistence-spec-3.1.html') },
+                            { label: 'JPA + Spring Boot Tutorial', action: () => window.open('https://www.baeldung.com/the-persistence-layer-with-spring-data-jpa') },
+                            { label: 'QueryDSL OpenFeign', action: () => window.open('https://github.com/OpenFeign/querydsl/') },
+                            { label: 'QueryDSL OpenFeign + Spring Boot Tutorial', action: () => window.open('https://www.baeldung.com/spring-cloud-openfeign') },
+                            { label: 'QueryDSL + Spring Boot Tutorial', action: () => window.open('https://www.baeldung.com/querydsl-with-jpa-tutorial') },
+                            { label: 'JOOQ', action: () => window.open('https://www.jooq.org/') },
+                            { label: 'JOOQ Docs', action: () => window.open('https://www.jooq.org/doc/latest/manual/') },
+                            { label: 'JOOQ + Spring Boot Tutorial', action: () => window.open('https://www.baeldung.com/jooq-with-spring') },
+                            { label: 'Jackson', action: () => window.open('https://github.com/FasterXML/jackson') },
+                            { label: 'Jackson Docs', action: () => window.open('https://github.com/FasterXML/jackson-docs') },
+                            { label: 'Lombok', action: () => window.open('https://projectlombok.org/') },
+                            { label: 'Lombok Docs', action: () => window.open('https://projectlombok.org/features/all') },
+                            { label: 'MapStruct', action: () => window.open('https://mapstruct.org/') },
+                            { label: 'MapStruct Docs', action: () => window.open('https://mapstruct.org/documentation/') },
+                            { label: 'MapStruct + Lombok Integration', action: () => window.open('https://mapstruct.org/documentation/stable/reference/html/#lombok') },
+                            { label: 'MapStruct + Spring Boot Tutorial', action: () => window.open('https://www.baeldung.com/mapstruct') }
+
+                        ]
+                    }
+
+
+
+                ]
+            } ,
             ] as MayoDevelopResource[];
     }
 
